f46b29923490b1cbd01dedc6f3b7b1f12b871fd0
Skyler Daché <skyberskyman@gmail.com>
2015-09-12 20:37:10 -0400

    removed git conflict markers

--- med_Icaps/med_Icaps/settings.py
+++ med_Icaps/med_Icaps/settings.py
@@ -38,18 +38,12 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'p_cal',
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
     'calendarium',
     'filer',
     'easy_thumbnails',
     'mptt'
-=======
 	'schedule',
 	'djangobower',
->>>>>>> origin/master
->>>>>>> a03f21a447c513d2eb04241650d52544deeefb32
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,11 +70,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-<<<<<<< HEAD
                 'django.core.context_processors.request',
-=======
 				"django.core.context_processors.request"
->>>>>>> origin/master
             ],
         },
     },
